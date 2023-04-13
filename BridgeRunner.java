a21974e98d969f0b3da81425b92ece07086182ba
Emilee Oquist <emiwemi495@gmail.com>
2023-04-13 01:09:24 -0700

    car

--- BridgeRunner.java
+++ BridgeRunner.java
@@ -3,15 +3,20 @@
  */
 
 public class BridgeRunner {
+	// Purely for testing purposes
+	private int test_val = 0;
 
 	public static void main(String[] args) {
-		// check command line inputs
 		int bridge_limit;
 		int num_cars;
+
 		if (args.length != 2) {
-            System.out.println("Usage: javac BridgeRunner <bridge limit> <num cars>");
-            return;
-        } else{
+			System.out.println("Usage: javac BridgeRunner <bridge limit> <num cars>");
+			return;
+		} else if (Integer.parseInt(args[0]) <= 0 || Integer.parseInt(args[1]) <= 0) {
+			System.out.println("Error: bridge limit and/or num cars must be positive.");
+			return;
+		} else {
 			bridge_limit = Integer.parseInt(args[0]);
 			num_cars = Integer.parseInt(args[1]);
 		}
@@ -19,11 +24,66 @@ public class BridgeRunner {
 		// instantiate the bridge
 		OneLaneBridge bridge = new OneLaneBridge(bridge_limit);
 
+		// ----- ----- ----- ----- -----
+		// ----- ----- ----- ----- -----
+
 		// TODO - allocate space for threads
+		Thread[] threads = new Thread[num_cars];
+		for (int i = 0; i < num_cars; i++) {
+			// Creating Thread objects -- need to encapsulate a Runnable object
+			int car_id = i;
+			threads[i] = new Thread(new Car(car_id, bridge));
+			
+			// Not a typo -- call start(), not run()
+			threads[i].start();
+		}
+
+		// TODO - actual thread stuff here
 
 		// TODO - start then join the threads
+		for (int i = 0; i < num_cars; i++) {
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// ----- ----- ----- ----- -----
+		// ----- ----- ----- ----- -----
 
 		System.out.println("All cars have crossed!!");
 	}
+
+	// ======================================================================================
+	// ======================================================================================
+	// ======================================================================================
+
+	public synchronized void inc() {
+		// do stuff with Counter object locked
+		test_val++;
+		// unlock right before leaving method
+	}
+
+	public int test_val() {
+		// this method doesn't require synchronization
+		// threads don't compete for the monitor lock
+		return test_val;
+	}
+
+	// You can use a synchronized block if only a critical section of code within
+	// the method needs locked.
+	// synchronized(objectToLockUp){
+	// // acquires intrinsic lock on objectToLockUp
+	// // releases lock after you leave the block
+	// }
+
+	// Three operations (API for Object class):
+	// • wait() - Block until another thread calls notify() or notifyAll()
+	// - Calling this releases the monitor lock and sleeps atomically!
+	// • notify() - Wake up one thread that's waiting
+	// • notifyAll() - Wake up all threads that's waiting
+	// Important: You must throw a synchronized block around these calls, or these
+	// calls must occur in a synchronized method.
 
 }
--- OneLaneBridge.java
+++ OneLaneBridge.java
@@ -23,22 +23,52 @@ public class OneLaneBridge extends Bridge {
 
     /**
      * Handles the arrival of a car to the bridge.
+     * 
      * @param car the arriving car
      * @throws InterruptedException if the thread is interrupted
      */
     @Override
     public void arrive(Car car) throws InterruptedException {
-        // TODO
+        // TODO - OneLaneBridge arrive()
+
+        // This method has to determine whether the thread which called it must wait, or
+        // is allowed to proceed on to the bridge. Specifically, a car can’t enter the
+        // bridge when there are too many cars on it or if it’s going against the
+        // current flow of traffic. When the car is allowed to enter the bridge, use the
+        // car’s setEntryTime(currentTime) method to set the entry time. Add the car to
+        // the bridge list. Then print the bridge list so we can see (and ensure) that
+        // there are no more than 3 cars on the bridge. Finally, increment currentTime
+        // by 1.
+
+        // OUTPUT: shows bridge’s current allowed direction, followed by the list of cars currently on the bridge.
     }
 
     /**
      * Handles the exit of a car leaving the bridge.
+     * 
      * @param car the exiting car
      * @throws InterruptedException if the thread is interrupted
      */
     @Override
     public void exit(Car car) throws InterruptedException {
-        // TODO
+        // TODO - OneLaneBridge exit()
+
+        // This Bridge method is called by a car when it wants to exit the bridge. But
+        // any car can call this method at any time, so we have to put in some “guard
+        // rails” to make sure that a car on the bridge can’t just randomly disappear
+        // off the bridge when there are still cars in front of it.
+
+        // When it’s the car’s turn to exit the bridge, remove the car from the list.
+        // Then print the bridge list so that we can see there’s one fewer car (and the
+        // car that left had better been at the head of the list!). When a car leaves,
+        // signal to other cars that might be waiting to get on the bridge. Do not
+        // change currentTime when exiting.
+
+        // OUTPUT: shows bridge’s current allowed direction, followed by the list of cars currently on the bridge.
+
     }
+
+
+    // TODO - check outputs section of notes
 
 }
